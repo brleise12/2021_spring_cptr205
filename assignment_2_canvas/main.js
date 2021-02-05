7233e091757c5326886e719c57007f31864d21d2
brleise12 <77751473+brleise12@users.noreply.github.com>
2021-02-05 13:42:04 -0600

    worked on assignment 2

--- assignment_2_canvas/main.js
+++ assignment_2_canvas/main.js
@@ -4,7 +4,8 @@ let canvas = document.querySelector('canvas');
 
 let context = canvas.getContext('2d'); 
 
-context.fillRect(50, 75, 10, 10); 
-
 context.fillStyle = 'green'; 
 context.fillRect(100, 175, 109, 100); 
+
+context.fillStyle = 'blue'; 
+context.fillRect(0, 0, 200, 100); 
